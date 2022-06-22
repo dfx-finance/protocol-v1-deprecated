6364561ff3d1d26fc483c395b8460ecd20c2fa95
tommy4241 <tommychan4241@gmail.com>
2022-06-23 00:21:47 +0800

    removal of virtual modifier

--- contracts/interfaces/ICurveFactory.sol
+++ contracts/interfaces/ICurveFactory.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.7.3;
 
 interface ICurveFactory {
-    function getProtocolFee () virtual external view returns (int128);
-    function getProtocolTreasury () virtual external view returns (address); 
+    function getProtocolFee () external view returns (int128);
+    function getProtocolTreasury () external view returns (address); 
 }
